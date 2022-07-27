6b1799212bd161d4603d2a45654514a1ae1908b3
Dexter <96929816+thedexter46@users.noreply.github.com>
2022-07-27 11:50:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ API_HASH = os.environ.get("API_HASH", "5b1d0992294a67cb54512a4fafeb0c88")
 API_ID = os.environ.get("API_ID", "6170803")
 # About Bot
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "1905228806:AAH2iahJcg5J6bqpcW11jB9KajwHbIjaslGTY")
-PICS = os.environ.get("PICS", "https://telegra.ph/file/034d53b5ed1d920ecab8b.jpg")
+PICS = os.environ.get("PICS")
 # Database
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "LuciferMoringstar-Robot")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://{Username}:{Passs}@cluster0.{clusterID}.mongodb.net/myFirstDatabase?retryWrites=true{iD}=majority")
